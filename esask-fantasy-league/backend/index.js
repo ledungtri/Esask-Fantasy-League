1f09df28af749f06dc8e9b47f6f4641de8b59e82
Bill Le <le2010@saskpolytech.ca>
2021-09-16 10:08:41 -0600

    setup server app

---
+++ esask-fantasy-league/backend/index.js
@@ -0,0 +1,31 @@
+const database = require('./database/database');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+function startServer() {
+    database.connect().then(() => {
+        app.use(express.json());
+        app.use(cors(
+            {origin: 'http://localhost:3000'}
+        ));
+
+        useRoutes();
+
+        const port = 3000;
+        app.listen(port, () => console.log(`Listening in port ${port}...`));
+    });
+}
+
+function useRoutes() {
+    // app.use('/api/teams', teamRoute);
+    // TODO: implement teams route
+    // TODO: implement player route
+    // TODO: implement contest route
+    // TODO: implement user route
+}
+
+startServer();
+
+module.exports = app;
