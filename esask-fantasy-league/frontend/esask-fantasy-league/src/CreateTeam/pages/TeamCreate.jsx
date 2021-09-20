2caaa2934d70d4d441d54918ef4dab188efaef67
Bill Le <le2010@saskpolytech.ca>
2021-09-20 09:32:10 -0600

    Refactor mock data

--- esask-fantasy-league/frontend/esask-fantasy-league/src/CreateTeam/pages/TeamCreate.jsx
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/CreateTeam/pages/TeamCreate.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import styled from 'styled-components';
+import apis from "../../api/api";
 import ContestDetails from "../components/ContestDetails";
 
 const Label = styled.label`
@@ -7,6 +8,16 @@ const Label = styled.label`
 `
 
 function TeamCreate() {
+    const mockContest = {name: "Contest Number 1", startDate: "17/09/2021", endDate: "24/09/2021"};
+    const mockPlayers = [
+        {"id": "wr37PIyFXfCAaTU6HaAxlIx-glt2GrpqUzoThzFGGb71QWE", "name": "nifty jg", "value": 0},
+        {"id": "uBgj79ORr0ilRONyu1MSEWqVUHH4feQ2mu4tzwUIUsFUQTU", "name": "Fated Temper", "value": 0},
+        {"id": "AmV1jV6kp7ecoMQIvA9pWwBrvOPi_4T22k1nDXPMVZM9xZ4", "name": "KatEvolved", "value": 0},
+        {"id": "MXcrR6nV-lsJOuN1LD-eBv3Hy6DP7jHjhTxUmtVYTA71bQQ", "name": "BOBtimer", "value": 0},
+        {"id": "WBXIbN2oRZpCNl31Mfrqvd7Mhg06FB4fT1OKgEF4_7VgzbQ", "name": "KryRa", "value": 0},
+        {"id": "hZWFpvpRVAmczqbF6gCwU6JvupAYJNmDVEYLFLwxFXItOnY-CTxyLjvVKA", "name": "HLE Arthur", "value": 0}
+    ];
+
     const [teamName, setTeamName] = useState();
     const handleChange = (e) => setTeamName(e.target.value);
 
@@ -16,23 +27,17 @@ function TeamCreate() {
         const payload = {
             "name": teamName,
             "contestId": "613d65ea63676a0636013920",
-            "players": [
-                {"id": "wr37PIyFXfCAaTU6HaAxlIx-glt2GrpqUzoThzFGGb71QWE", "name": "nifty jg", "value": 0},
-                {"id": "uBgj79ORr0ilRONyu1MSEWqVUHH4feQ2mu4tzwUIUsFUQTU", "name": "Fated Temper", "value": 0},
-                {"id": "AmV1jV6kp7ecoMQIvA9pWwBrvOPi_4T22k1nDXPMVZM9xZ4", "name": "KatEvolved", "value": 0},
-                {"id": "MXcrR6nV-lsJOuN1LD-eBv3Hy6DP7jHjhTxUmtVYTA71bQQ", "name": "BOBtimer", "value": 0},
-                {"id": "WBXIbN2oRZpCNl31Mfrqvd7Mhg06FB4fT1OKgEF4_7VgzbQ", "name": "KryRa", "value": 0},
-                {"id": "hZWFpvpRVAmczqbF6gCwU6JvupAYJNmDVEYLFLwxFXItOnY-CTxyLjvVKA", "name": "HLE Arthur", "value": 0}
-            ]
+            "players": mockPlayers
         };
-    }
 
-    const contest = {name: "Contest Number 1", startDate: "17/09/2021", endDate: "24/09/2021"};
+        apis.createTeam(payload).then(res => {
+            console.log("Team created successfully");
+        });
+    }
 
     return (
         <div className="TeamCreateContainer">
-
-            <ContestDetails contest={contest}/>
+            <ContestDetails contest={mockContest}/>
 
             <form className="createTeamForm" onSubmit={handleSubmit}>
                 <Label>Team Name:</Label>
