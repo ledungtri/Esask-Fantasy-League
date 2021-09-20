2cc2dd06e27e4ee3b7b5b426b3bd284d524d79c8
Oreoluwa Olaleye <olaleye0285@saskpolytech.ca>
2021-09-20 12:33:32 -0600

    update to title,playerlist, playersperpage, styles

--- esask-fantasy-league/frontend/esask-fantasy-league/src/App.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './Home Page/components/HomePage';
+import Navbar from './NavBar/components/Navbar';
 import PlayerList from './Player List/components/PlayerList';
 
 function App() {
   return (
     <Router>
         <div className="App">
+          <Navbar />
           <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/players" exact component={PlayerList} />
