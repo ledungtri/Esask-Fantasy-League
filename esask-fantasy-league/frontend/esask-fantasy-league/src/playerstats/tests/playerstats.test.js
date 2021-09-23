1c38a919b3a6bfe9bb049ef23363bfeddbb3c1ee
nzbadi <zbadi7899@saskpolytech.ca>
2021-09-23 11:55:55 -0600

    added test for button click

--- esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/tests/playerstats.test.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/tests/playerstats.test.js
@@ -21,3 +21,25 @@ describe('The button to draft the player shows up only when user is logged in', 
 
 });
 
+it('Closes the popup when the button is clicked', async  () => {
+
+  
+  render(<Playerstats loggedin={true} />);
+  const button = screen.getByText('Draft this player');
+  await waitFor(()=>screen.getByText('Draft this player'));
+  fireEvent.click(screen.getByText('Draft this player'));
+  await waitForElementToBeRemoved(screen.getByText('Draft this player'));
+  expect(button).not.toBeInTheDocument();
+});
+
+
+it('Calls the function on draft button click ', async  () => {
+  const onClick = jest.fn();
+  render(<DraftButton onClose={onClick} loggedin={true} />);
+  await waitFor(()=>screen.getByText('Draft this player'));
+  //render(<button onClick={onClick} />);
+  fireEvent.click(screen.getByText('Draft this player'));  
+  expect(onClick).toHaveBeenCalled();
+});
+
+
