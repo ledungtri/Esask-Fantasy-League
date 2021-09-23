ca8258b186d3fca72868b031956b268acc233970
nzbadi <zbadi7899@saskpolytech.ca>
2021-09-23 10:27:31 -0600

    updated tests

--- esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/tests/playerstats.test.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/tests/playerstats.test.js
@@ -8,7 +8,10 @@ import '@testing-library/jest-dom'
 
 
 describe('The button to draft the player shows up only when user is logged in', () => {
-
+  it('Not to render the button when logged out ', () => {
+    render(<Playerstats loggedin={false}/>);
+    expect(screen.queryByTestId(/draftbtn/i)).not.toBeTruthy();
+  });
 
   it('Render the draft button when user is logged in ', () => {
     render(<Playerstats loggedin={true}/>);
@@ -18,7 +21,36 @@ describe('The button to draft the player shows up only when user is logged in', 
 
 });
 
+it('Closes the popup when the button is clicked', async  () => {
+
+  
+  render(<Playerstats loggedin={true} />);
+  const button = screen.getByText('Draft this player');
+  await waitFor(()=>screen.getByText('Draft this player'));
+  fireEvent.click(screen.getByText('Draft this player'));
+  await waitForElementToBeRemoved(screen.getByText('Draft this player'));
+  expect(button).not.toBeInTheDocument();
+});
 
 
+it('Calls the function on draft button click ', async  () => {
+  const onClick = jest.fn();
+  render(<DraftButton onClose={onClick} loggedin={true} />);
+  await waitFor(()=>screen.getByText('Draft this player'));
+  //render(<button onClick={onClick} />);
+  fireEvent.click(screen.getByText('Draft this player'));  
+  expect(onClick).toHaveBeenCalled();
+});
 
 
+it('gets the data from the server', async  () => {
+
+ // jest.useFakeTimers('legacy');
+  render(<Playerstats loggedin={true} />);
+  //jest.setTimeout(20000);
+  await waitFor(()=>screen.queryByTestId(/loadingtext/i));
+ // const loadingText = screen.queryByTestId(/loadingtext/i);
+  await new Promise((resolve) => setTimeout(resolve, 10000)); // 1000 is the time that i know my `msw` should be finished.
+  await waitForElementToBeRemoved(screen.queryByTestId(/loadingtext/i));
+  expect(loadingText).not.toBeInTheDocument();
+}, 220000);
