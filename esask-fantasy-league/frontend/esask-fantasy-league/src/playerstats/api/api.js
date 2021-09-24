167606d6ae44257b3fa3c014126de86a86e2b567
Bill Le <le2010@saskpolytech.ca>
2021-09-24 11:37:57 -0600

    use backendHost.BACKEND_HOST

--- esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/api/api.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/playerstats/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import backendHost from "../../api/backendHost";
 export async function getData(sumonnerIDProp) {
     const response = await axios.get(
-        'http://localhost:3001/api/player/'+ sumonnerIDProp, {
+        backendHost.BACKEND_HOST + '/api/player/'+ sumonnerIDProp, {
           headers: {
 
           }
