8f4bd97a8ddbad9aee8f9328c076e9bf5eea05ff
Oreoluwa Olaleye <olaleye0285@saskpolytech.ca>
2021-09-29 18:19:04 -0600

    search bar component

--- esask-fantasy-league/frontend/esask-fantasy-league/src/Search Bar/components/SearchBar.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/Search Bar/components/SearchBar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function SearchBar() {
+function SearchBar(props) {
+    const [searchText, setSearchText] = useState("");
+
+
     return (
-        <div>
-            
+        <div className="searchBar">
+            <div className="searchInput_div">
+                <input data-testid="test_search_field" type="text" name="input_text" value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search players by name" />
+                
+            </div>
+
+            {props.showError ? <span data-testid="test_error_span" className="searchError">Enter search criteria</span> : ""}
+
+            <div className="searchBtn_div">
+                <button data-testid="test_search_btn" onClick={() => props.getSearchText(searchText)}>Search</button>
+                <button data-testid="test_search_btn2" onClick={props.clear}>Cancel</button>
+            </div>
         </div>
     )
 }
