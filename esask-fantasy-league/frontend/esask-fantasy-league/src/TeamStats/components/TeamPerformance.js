7d9f81f81c83c9b0195bd1b79795d26028e22805
nzbadi <zbadi7899@saskpolytech.ca>
2021-10-03 15:01:35 -0600

    Team's stats operational

--- esask-fantasy-league/frontend/esask-fantasy-league/src/TeamStats/components/TeamPerformance.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/TeamStats/components/TeamPerformance.js
@@ -11,18 +11,6 @@ export default function TeamPerformance(props) {
     return (
 
         <>
-
-        {/* <div className="container">
-            <div className="row performance-row" >
-                <div className="col text-light performance-col"> Towers : {performance? performance.towers:""} </div>
-                <div className="col text-light performance-col"> Dragons : {performance? performance.dragons:""} </div>
-                <div className="col text-light performance-col"> Barons : {performance? performance.barons:""} </div>
-                <div className="col text-light performance-col"> Wins : {performance? performance.wins:""} </div>
-                <div className="col text-light performance-col"> Captain Bonus Score : {performance? performance.captainBonusScore:""} </div>
-                <div className="w-100"></div>
-  
-            </div>
-        </div> */}
         <h3 className="text-center text-light">Captain Bonuses:</h3>
 
         <Table striped bordered hover variant="dark">
