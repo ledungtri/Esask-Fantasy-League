82ed9d0ab4c4b7f74c113562fb72bb91b7d4c9fe
Oreoluwa Olaleye <olaleye0285@saskpolytech.ca>
2021-10-05 15:07:40 -0600

    updated text in loading component to props.message

--- esask-fantasy-league/frontend/esask-fantasy-league/src/Player List/components/Loading.js
+++ esask-fantasy-league/frontend/esask-fantasy-league/src/Player List/components/Loading.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Loading() {
+function Loading(props) {
     return (
         <div data-testid="load_comp">
             <div className="preloader">
                 <div className="spinner">
                     <div className="half_spinner"></div>
-                    <p>Loading.....</p>
+                    <p>{props.message}</p>
                 </div>
             </div>
         </div>
